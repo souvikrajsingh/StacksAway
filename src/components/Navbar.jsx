c988c119c5513fbe904b512a1a8d3dfcc0bc3c77
souvikrajsingh <souvikrajsingh@gmail.com>
2024-03-23 18:19:49 +0530

    minor-link-fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,14 @@ function Navbar() {
       <nav className="px-0 sm:px-6 pt-1 pb-2 xl:pb-4 2xl:pb-6  border-gray border-opacity-20 mt-4 xl:mt-8 2xl:mt-10  flex items-center justify-between">
         <div className="flex justify-start gap-4">
           <Logosimplewhite />
-          <Logofull />
+          <Link to="/">
+            <h1
+              className="font-bold text-h2 font-cabinG"
+              style={{ color: "#D0C7B3" }}
+            >
+              STACKSAWAY
+            </h1>
+          </Link>
         </div>
         <div className="flex items-center gap-x-8">
           <span className="font-medium hidden sm:block relative overflow-hidden group h-fit text-base xl:text-h6 2xl:text-h5">
